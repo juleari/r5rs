71f76f7229bddeb90555869d7d201c782115a6b9
juleari <juleari@yandex.ru>
2015-10-06 20:36:04 +0300

    fact 0 fix

--- lab15 memo lazy.scm
+++ lab15 memo lazy.scm
@@ -32,17 +32,20 @@
 (define (lazy-factorial n)
   (define (n! x y)
     (let ((x! (* y x)))
-      (lazy-cons x! (n! (+ x 1) x!)))
-    (lazy-ref (n! 1 1) n)))
+      (lazy-cons x! (n! (+ x 1) x!))))
+  (if (zero? n)
+      1
+      (lazy-ref (n! 1 1) n)))
 
+#|
 ;; tests
 (begin
   (display (memoized-factorial 10)) (newline)
-  (display (memoized-factorial 50)) (newline))
-
-(display (lazy-head (naturals 10) 12))
-(display (lazy-ref  (naturals 20) 12))
-
-(begin
+  (display (memoized-factorial 50)) (newline)
+  
+  (display (lazy-head (naturals 10) 12)) (newline)
+  (display (lazy-ref  (naturals 20) 12)) (newline)
+  
   (display (lazy-factorial 10)) (newline)
   (display (lazy-factorial 50)) (newline))
+|#

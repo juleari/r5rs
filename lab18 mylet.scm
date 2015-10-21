e92ea5a30bd488094a65c9415d9e657babd09259
juleari <juleari@yandex.ru>
2015-10-22 00:07:36 +0300

    my-let

--- lab18 mylet.scm
+++ lab18 mylet.scm
@@ -1,27 +1,20 @@
 ;(use-syntax (ice9-syncase))
 
-#|(define (get-kv% kv)
-  (define (helper kv k v)
-    (if (null? kv)
-        (list (reverse k) (reverse v))
-        (helper (cdr kv) (cons (caar kv) k) (cons (cadar kv) v))))
-  (helper kv '() '()))
-
-(define (my-let% kv body)
-  (write (car kv))
-  (newline)
-  (write (cadr kv))
-  (newline)
-  (apply (lambda (car kv) (begin body)) (cadr kv)))|#
-
 (define-syntax my-let
   (syntax-rules ()
-    ((_ ((k1 v1)) body ...)             ((lambda (k1) body ...) v1))
-    ((_ ((k1 v1) (k2 v2) ...) body ...) ((lambda (k1) (my-let ((k2 v2) ...) body ...)) v1))))
-
-(my-let ((a 1)(b (+ 7 2))) (- a b)(+ a b))
+    ((_ ((k1 v1)) body ...)                             ((lambda (k1) body ...) v1))
+    ((_ (k ...) (v ...) ((k1 v1)) body ...)             ((lambda (k ... k1) body ...) v ... v1))
+    ((_ (k ...) (v ...) ((k1 v1)) body ...)             ((lambda (k ... k1) body ...) v ... v1))
+    ((_ (k ...) (v ...) ((k1 v1) (k2 v2) ...) body ...)
+     (my-let (k ... k1) (v ... v1) ((k2 v2) ...) body ...))
+    ((_ ((k1 v1) (k2 v2) ...) body ...)
+     (my-let (k1) (v1) ((k2 v2) ...) body ...))))
 
 (define-syntax my-let*
   (syntax-rules ()
     ((_ ((k1 v1)) body ...)             ((lambda (k1) body ...) v1))
     ((_ ((k1 v1) (k2 v2) ...) body ...) ((lambda (k1) (my-let* ((k2 v2) ...) body ...)) v1))))
+
+;; tests
+#|(my-let ((a 1)(b 2)(c 3)) (+ a 5)(+ a b c))
+(my-let* ((a 1)(b (+ a 2))) (- a b)(+ a b))|#

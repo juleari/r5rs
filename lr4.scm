e13e91b10384558650abb370fbd40c16e3229b62
juleari <juleari@yandex.ru>
2015-10-30 17:19:36 +0300

    lr4 forth interpretate

---
+++ lr4.scm
@@ -0,0 +1,124 @@
+(define (interpret program stack)
+  (define (op? x)
+    (or (eq? x '+) (eq? x '-) (eq? x '*) (eq? x '/) (eq? x 'or) (eq? x 'and)))
+
+  (define (comp? x)
+    (or (eq? x '=) (eq? x '<) (eq? x '>)))
+  
+  (define (scan i stack return dict)
+    (if (eq? i (vector-length program))
+        stack
+        (let* ((word (vector-ref program i))
+               (func (assoc word dict)))
+                ;; Выполнение программы ;;
+          (cond (func (scan (cadr func) stack (cons (+ i 1) return) dict))
+                ;; Число на стек ;;
+                ((number? word) (scan (+ 1 i) (cons word stack) return dict))
+                ;; Управляющие конструкции ;;
+                ((eq? word 'define) (let loop ((ind (+ 2 i)))
+                                      (if (eq? (vector-ref program ind) 'end)
+                                          (scan (+ 1 ind) stack return
+                                                (cons (list (vector-ref program (+ 1 i)) (+ 2 i))
+                                                      dict))
+                                          (loop (+ 1 ind)))))
+                ((eq? word 'end) (scan (car return) stack (cdr return) dict))
+                ((eq? word 'exit) (scan (car return) stack (cdr return) dict))
+                ((eq? word 'if) (if (zero? (car stack))
+                                    (let loop ((ind (+ 1 i)))
+                                      (if (eq? (vector-ref program ind) 'endif)
+                                          (scan (+ 1 ind) (cdr stack) return dict)
+                                          (loop (+ 1 ind))))
+                                    (scan (+ 1 i) (cdr stack) return dict)))
+                ((eq? word 'endif) (scan (+ 1 i) stack return dict))
+                ;; Операции с числами ;;
+                ((op? word) (scan (+ 1 i) 
+                                  (eval `(cons (,word (cadr (list ,@stack)) (car (list ,@stack)))
+                                               (cddr (list ,@stack)))
+                                        (interaction-environment))
+                                  return dict))
+                ((eq? word 'mod) (scan (+ 1 i)
+                                       (cons (remainder (cadr stack) (car stack)) (cddr stack))
+                                       return dict))
+                ((eq? word 'neg) (scan (+ 1 i) (cons (- (car stack)) (cdr stack)) return dict))
+                ((comp? word) (scan (+ 1 i) 
+                                  (eval `(cons (if (,word (cadr (list ,@stack))
+                                                        (car (list ,@stack))) -1 0)
+                                               (cddr (list ,@stack)))
+                                        (interaction-environment))
+                                  return
+                                  dict))
+                ((eq? word 'not) (scan (+ 1 i) (cons (not (car stack)) (cdr stack)) return dict))
+                ;; Операции со стеком ;;
+                ((eq? word 'drop) (scan (+ 1 i) (cdr stack) return dict))
+                ((eq? word 'swap) (scan (+ 1 i) (append (list (cadr stack) (car stack))
+                                                        (cddr stack)) return dict))
+                ((eq? word 'dup)  (scan (+ 1 i) (cons (car stack) stack) return dict))
+                ((eq? word 'over) (scan (+ 1 i) (cons (cadr stack) stack) return dict))
+                ((eq? word 'rot)  (scan (+ 1 i) (append (list (caddr stack) (cadr stack) (car stack))
+                                                        (cdddr stack)) return dict))
+                ((eq? word 'depth) (scan (+ 1 i) (cons (length stack) stack) return dict))))))
+  (scan 0 stack '() '((#f #f))))
+
+;; tests
+#|(interpret #(define abs
+              dup 0 <
+              if neg endif
+             end
+              9 abs
+              -9 abs) '())
+
+(interpret #(   define =0? dup 0 = end 
+                define <0? dup 0 < end 
+                define signum 
+                    =0? if exit endif 
+                    <0? if drop -1 exit endif 
+                    drop 
+                    1 
+                end 
+                 0 signum 
+                -5 signum 
+                10 signum       ) (quote ()))
+
+(interpret #(   define -- 1 - end 
+                define =0? dup 0 = end 
+                define =1? dup 1 = end 
+                define factorial 
+                    =0? if drop 1 exit endif 
+                    =1? if drop 1 exit endif 
+                    dup -- 
+                    factorial 
+                    * 
+                end 
+                0 factorial 
+                1 factorial 
+                2 factorial 
+                3 factorial 
+                4 factorial     ) (quote ()))
+
+(interpret #(   define =0? dup 0 = end 
+                define =1? dup 1 = end 
+                define -- 1 - end 
+                define fib 
+                    =0? if drop 0 exit endif 
+                    =1? if drop 1 exit endif 
+                    -- dup 
+                    -- fib 
+                    swap fib 
+                    + 
+                end 
+                define make-fib 
+                    dup 0 < if drop exit endif 
+                    dup fib 
+                    swap -- 
+                    make-fib 
+                end 
+                10 make-fib     ) (quote ()))
+
+(interpret #(   define =0? dup 0 = end 
+                define gcd 
+                    =0? if drop exit endif 
+                    swap over mod 
+                    gcd 
+                end 
+                90 99 gcd 
+                234 8100 gcd    ) '())|#

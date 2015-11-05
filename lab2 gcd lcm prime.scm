88b2240087245cb95be56e48b0c5975579d54fec
juleari <juleari@yandex.ru>
2015-11-05 21:50:06 +0300

    zero?

--- lab2 gcd lcm prime.scm
+++ lab2 gcd lcm prime.scm
@@ -2,7 +2,7 @@
   (if (< a b)
       (my-gcd b a)
       (let ((r (remainder a b)))
-        (if (= 0 r)
+        (if (zero? r)
             b
             (my-gcd b r)))))
 
@@ -11,14 +11,15 @@
 
 (define (prime? n)
   (define (fact n)
-    (if (= n 0)
+    (if (zero? n)
         1
         (* n (fact (- n 1)))))
   
-  (= 0 (remainder (+ 1 (fact (- n 1))) n)))
+  (zero? (remainder (+ 1 (fact (- n 1))) n)))
 
 ;; tests
 (my-gcd 3542 2464)
 (my-lcm 3 4)
 (prime? 11)
 (prime? 12)
+(prime? 3571)

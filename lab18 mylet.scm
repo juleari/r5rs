001da870fcd3ce4b9915ca60c899c86c428d66da
juleari <juleari@yandex.ru>
2015-11-08 03:22:01 +0300

    -> ...

--- lab18 mylet.scm
+++ lab18 mylet.scm
@@ -2,12 +2,8 @@
 
 (define-syntax my-let
   (syntax-rules ()
-    ((_ ((k1 v1)) body ...)                             ((lambda (k1) body ...) v1))
-    ((_ (k ...) (v ...) ((k1 v1)) body ...)             ((lambda (k ... k1) body ...) v ... v1))
-    ((_ (k ...) (v ...) ((k1 v1) (k2 v2) ...) body ...)
-     (my-let (k ... k1) (v ... v1) ((k2 v2) ...) body ...))
-    ((_ ((k1 v1) (k2 v2) ...) body ...)
-     (my-let (k1) (v1) ((k2 v2) ...) body ...))))
+    ((_ ((k1 v1)) body ...)             ((lambda (k1) body ...) v1))
+    ((_ ((k1 v1) (k2 v2) ...) body ...) ((lambda (k1 k2 ...) body ...) v1 v2 ...))))
 
 (define-syntax my-let*
   (syntax-rules ()
